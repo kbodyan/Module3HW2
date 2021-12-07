2cdbedb7297b06c6d6166b90639c4e500cc607cb
Konstantin Bodyan <kbodyan@gmail.com>
2021-12-07 23:25:33 +0200

    transfer

---
+++ PhoneBook/PhoneBook/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string[] Phones { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{Name} {Surname}";
+            }
+        }
+    }
+}
---
+++ PhoneBook/PhoneBook/PhoneCollection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    public class PhoneCollection : ICollection<Contact>
+    {
+        private SortedDictionary<char, SortedDictionary<string, Contact>> _book = new SortedDictionary<char, SortedDictionary<string, Contact>>();
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(Contact item)
+        {
+            if (item != null && string.IsNullOrEmpty(item.FullName))
+            {
+                var letter = item.FullName[0];
+                if (!_book.ContainsKey(letter))
+                {
+                    _book.Add(letter, new SortedDictionary<string, Contact>());
+                }
+
+                _book[letter].Add(item.FullName, item);
+                else if (item != null && !_book.ContainsKey('#'))
+                {
+                    _book.Add('#', new SortedDictionary<string, Contact>());
+                }
+            }
+            
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
